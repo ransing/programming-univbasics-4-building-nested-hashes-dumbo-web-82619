bcbebf5d4d681118aad67c828a7da9d8d40b16e5
virajransing <virajransing@gmail.com>
2019-08-23 14:23:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,24 +1,40 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-	{}
+  hash = {:railroads => {}}
+  hash 
 end
 
 def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  
+  railroads= {:railroads => {:pieces =>4}}
+  
+  
 end
 
-def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+def monopoly_with_third_tier 
+  hash = {:railroads => {:names =>  {:pen_railroad =>{}, :syd_railroad =>{}, :bro_railroad =>{}, :mh_railroad =>3},:pieces =>4,:rent_in_dollars => { :one_piece_owned=>25,
+      :two_pieces_owned=>50,
+      :three_pieces_owned=>100,
+      :four_pieces_owned=>200}}}
+  # hash[:rent_in_dollars] = 3
+  # hash[:railroads][:rent_in_dollars] = 3 
+  
+  # hash [:one_piece_owned] = 25
+  # hash [:two_pieces_owned] = 50 
+  # hash [:three_pieces_owned] = 100
+  # hash [:four_pieces_owned] = 200 
+  
+  
+  
+  
+  # {:railroads => {:names =>{},{:rent_in_dollars => {:one_piece_owned=>25,
+  #     :two_pieces_owned=>50,
+  #     :three_pieces_owned=>100,
+  #     :four_pieces_owned=>200}}}
+  
 end
 
 def monopoly_with_fourth_tier
+  {:railroads => {:rent_in_dollars => {}}}
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
